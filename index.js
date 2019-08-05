98f9e3f24cd372b12d39ffb6aaef7f59f6d92338
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2019-08-05 20:20:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-'use strict'
-
-const CATEGORY_GLOBAL = 9999,
-	SKILL_FLYING_DISMOUNT = 65000001
-
-module.exports = function FlyForever(mod) {
+module.exports = function fly-forever(mod) {
+	const CATEGORY_GLOBAL = 9999
+	const SKILL_FLYING_DISMOUNT = 65000001
+	
 	let gameId = -1n,
 		location = null,
 		outOfEnergy = false,
@@ -13,60 +11,83 @@ module.exports = function FlyForever(mod) {
 		mountSkill = -1,
 		serverMounted = false,
 		remountTimer = null
-
-	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => { ({gameId} = event) })
-
-	mod.hook('S_CANT_FLY_ANYMORE', 'raw', () => false)
-	mod.hook('S_PLAYER_CHANGE_FLIGHT_ENERGY', 1, event => { outOfEnergy = event.energy === 0 })
-
-	mod.hook('C_PLAYER_LOCATION', 5, event => { location = {flying: false, pos: event.loc, dir: event.w} })
-	mod.hook('C_PLAYER_FLYING_LOCATION', 4, event => {
-		location = {flying: true, pos: event.loc, dir: event.w}
-		if(outOfEnergy && event.type !== 7 && event.type !== 8) {
+	
+	mod.hook('C_PLAYER_FLYING_LOCATION', 4, (event) => {
+		location = {
+			flying: true,
+			pos: event.loc,
+			dir: event.w
+		}
+		
+		if (outOfEnergy && event.type !== 7 && event.type !== 8) {
 			event.type = 7
 			return true
 		}
 	})
-
-	mod.hook('S_SKILL_CATEGORY', 3, event => { if(event.category === CATEGORY_GLOBAL) mountDisabled = !event.enabled })
-	mod.hook('S_USER_STATUS', 2, event => { if(event.gameId === gameId) inCombat = event.status === 1 })
-
-	mod.hook('C_START_SKILL', 7, {order: -10}, event => {
-		if(event.skill.id === mountSkill || event.skill.id === SKILL_FLYING_DISMOUNT) {
+	
+	mod.hook('C_PLAYER_LOCATION', 5, (event) => {
+		location = {
+			flying: false,
+			pos: event.loc,
+			dir: event.w
+		}
+	})
+	
+	mod.hook('C_START_SKILL', 7, (event) => {
+		if (event.skill.id == mountSkill || event.skill.id == SKILL_FLYING_DISMOUNT) {
 			dismountByUser = true
 			mountSkill = -1
 		}
 	})
-
-	mod.hook('S_MOUNT_VEHICLE', 2, {order: 10}, event => {
-		if(event.gameId === gameId) {
+	
+	mod.hook('S_CANT_FLY_ANYMORE', 1, (event) => {
+		return false
+	})
+	
+	mod.hook('S_MOUNT_VEHICLE', 2, {order: 10}, (event) => {
+		if (event.gameId == mod.game.me.gameId) {
 			const fakeMounted = mountSkill !== -1
-
 			serverMounted = true
 			mountSkill = event.skill
-
-			if(fakeMounted) return false
+			if (fakeMounted) {
+				return false
+			}
 		}
 	})
-
-	mod.hook('S_UNMOUNT_VEHICLE', 2, {order: 10}, event => {
-		if(event.gameId !== gameId) return
-
+	
+	mod.hook('S_PLAYER_CHANGE_FLIGHT_ENERGY', 1, (event) => {
+		outOfEnergy = (event.energy === 0)
+	})
+	
+	mod.hook('S_SKILL_CATEGORY', 3, (event) => {
+		if (event.category == CATEGORY_GLOBAL) {
+			mountDisabled = !event.enabled
+		}
+	})
+	
+	mod.hook('S_UNMOUNT_VEHICLE', 2, {order: 10}, (event) => {
+		if (event.gameId != mod.game.me.gameId) {
+			return
+		}
 		serverMounted = false
-
-		if(!location.flying || dismountByUser) {
+		if (!location.flying || dismountByUser) {
 			dismountByUser = false
 			mountSkill = -1
-		}
-		else {
+		} else {
 			clearTimeout(remountTimer)
 			remountTimer = setTimeout(tryRemount, 50)
 			return false
 		}
 	})
-
+	
+	mod.hook('S_USER_STATUS', 3, (event) => {
+		if (event.gameId == mod.game.me.gameId) {
+			inCombat = event.status == 1
+		}
+	})
+	
 	function tryRemount() {
-		if(!mountDisabled && !inCombat) {
+		if (!mountDisabled && !inCombat) {
 			mod.send('C_START_SKILL', 7, {
 				skill: mountSkill,
 				w: location.dir,
@@ -74,15 +95,21 @@ module.exports = function FlyForever(mod) {
 				unk: true
 			})
 			remountTimer = setTimeout(() => {
-				if(!serverMounted) {
-					mod.send('S_UNMOUNT_VEHICLE', 2, {gameId, skill: mountSkill})
+				if (!serverMounted) {
+					mod.send('S_UNMOUNT_VEHICLE', 2, {
+						gameId,
+						skill: mountSkill
+					})
 					mountSkill = -1
 				}
 			}, 1000)
-		}
-		else {
-			mod.send('S_UNMOUNT_VEHICLE', 2, {gameId, skill: mountSkill})
+		} else {
+			mod.send('S_UNMOUNT_VEHICLE', 2, {
+				gameId,
+				skill: mountSkill
+			})
 			mountSkill = -1
 		}
 	}
+	
 }
