e43181db5bdaa41281f41289ce24f1bb58ae943a
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2020-02-16 18:53:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ module.exports = function fly(mod) {
 	mod.hook('S_LOAD_TOPO', 3, e=>{
 		currZone = e.zone;
 	});
+
+	mod.hook('C_PLAYER_LOCATION', 5, e=>{
+		return !([2, 10].includes(e.type) && (currZone < 10 || currZone > 200));       
+	});
 	
 	mod.hook('C_PLAYER_LOCATION', 5, (event) => {
 		location = {
@@ -38,7 +42,6 @@ module.exports = function fly(mod) {
 			pos: event.loc,
 			dir: event.w
 		}
-		([2, 10].includes(e.type) && (currZone < 10 || currZone > 200));
 	})
 	
 	mod.hook('C_START_SKILL', 7, (event) => {
